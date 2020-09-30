f7d096e48d7dbf36300a0aa80331f751d2b62e12
gtm003 <70858746+gtm003@users.noreply.github.com>
2020-09-30 15:02:20 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min;
+    if (array == undefined || array.length == 0) min = 0;
+    else {
+        min = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < min) min = array[i];
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let max;
+    if (array == undefined || array.length == 0) max = 0;
+    else {
+        max = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > max) max = array[i];
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg, sum = 0;
+    if (array == undefined || array.length == 0) avg = 0;
+    else {
+        for (i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avg = sum / array.length;
+    }
+    return avg;
 }
